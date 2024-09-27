642e3bd395c4621e9e6126f35860f9615d898aa0
dnlslcd <danielsalcedoq@gmail.com>
2024-09-27 20:19:21 +0200

    Iteración 1 - hecha

--- app.js
+++ app.js
@@ -13,6 +13,13 @@ app.use(express.json());
 // DATABASE CONNECTION
 const mongoose = require('mongoose');
 
+const MONGODB_URI = "mongodb://127.0.0.1:27017/express-mongoose-recipes-dev";
+
+mongoose
+  .connect(MONGODB_URI)
+  .then((x) => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
+  .catch((err) => console.error("Error connecting to mongo", err));
+
 
 // ROUTES
 //  GET  / route - This is just an example route
